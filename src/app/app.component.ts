7e9524c2ee03e478f410e08b0cb8d26791c73a96
ildarvafin <rushana987@gmail.com>
2024-12-03 22:07:56 +0200

    add nev changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,10 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'mentoring-first-project';
+  title = 'mentoring-first';
 }
